f092a37e6694faa7e2dce24b386fa1390e5b11bf
Benjamin Cooper <bencooper222@gmail.com>
2017-01-21 16:59:28 -0500

    Boilerplae of basic app
    LET'S GO

---
+++ App/Phoneword/RestfulFun.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Phoneword
+{
+    class RestfulFun
+    {
+       public static void makeGETrequest(string location)
+        {
+            WebRequest wrGETURL = WebRequest.Create(location);
+            Stream objStream;
+            objStream = wrGETURL.GetResponse().GetResponseStream();
+
+            StreamReader objReader = new StreamReader(objStream);
+
+            string sLine = "";
+            int i = 0;  
+
+            while (sLine != null)
+            {
+                i++;
+                sLine = objReader.ReadLine();
+                if (sLine != null)
+                    Console.WriteLine("{0}:{1}", i, sLine);
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
